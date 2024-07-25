934ca233d84e9021120bb0d864d51aa44917a4e7
Amit Yaron <70226919+Amityaron@users.noreply.github.com>
2024-07-25 22:28:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import sys
+import os
+
+
+st.set_page_config(
+    page_title="Multi-App Navigation",
+    page_icon="🌟",
+)
+
+st.write("# Welcome to stock market detection👋")
+st.markdown(
+    
+    """
+    Stock market detection is an open-source app framework built specifically for
+    
+
+    Anomaly dectection in the stock market using Machine Learning and Data Science projects.
+    ### Streamlit App Identify the following :
+    - Long position based on volatility.
+    - The percentage of monthly change that help us annual planning in our financial investment.
+    - Sell call options with 90 percentage of success to get monthly premium.
+    
+    ### Z score app: 
+    - Comapre the volatility between etfs
+
+    ### Sell out of the money call options 
+    -  Sell out of the money call options of your securities with the probabilty you like ! 
+    
+"""
+)
+
+st.sidebar.title("Navigation")
+st.sidebar.markdown("## Select a page:")
+st.sidebar.success("Choose a page")
+
+
+
+
+
